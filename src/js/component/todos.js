2302837ddb2eab3f00d750af3b3bd06bb5b4db8c
Karla9191 <marietha2014@gmail.com>
2021-04-02 03:31:05 +0000

    last

--- src/js/component/todos.js
+++ src/js/component/todos.js
@@ -4,15 +4,10 @@ export function TodosList() {
 	let [todosList, setTodosList] = useState([]);
 
 	return (
-        <h1> Tareas por hacer </h1>
 		<div className="container">
-            <button className="btn btn-primary" type="button">OK</button>
-			<ul class="list-group">
-				<li class="list-group-item"></li>
-				<li class="list-group-item"></li>
-				<li class="list-group-item"></li>
-				<li class="list-group-item"></li>
-				<li class="list-group-item"></li>
+			<h1> Tareas por hacer </h1>
+			<ul className="list-group">
+				<li className="list-group-item"></li>
 			</ul>
 		</div>
 	);
