df11b025002c7b7ffa36e29d4f95efbd7c437075
Karla9191 <marietha2014@gmail.com>
2021-05-02 17:38:22 +0000

    cambios-diseño

--- src/js/component/todos.js
+++ src/js/component/todos.js
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-export function TodosList() {
+export function TodosList(props) {
 	let [todosList, setTodosList] = useState([]);
 
+	const handleChange = e => {
+		setTodosList(e.target.value);
+		console.log(handleChange);
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		console.log(todosList);
+		setTodosList("");
+	};
 	return (
-		<div className="container">
-			<h1> Tareas por hacer </h1>
-			<ul className="list-group">
-				<li className="list-group-item"></li>
-			</ul>
-		</div>
+		<form onSubmit={handleSubmit}>
+			<div className="container">
+				<h1> Tareas por hacer </h1>
+				<ul className="list-group">
+					<li className="list-group-item">
+						<input
+							placeholder="Ingrese una tarea"
+							value={todosList}
+							onChange={handleChange}
+							name="tarea"
+							className="todoAdd"
+						/>
+						<button onClick={handleSubmit} className="btnAgregar">
+							Agregar Tarea
+						</button>
+					</li>
+				</ul>
+			</div>
+		</form>
 	);
 }
